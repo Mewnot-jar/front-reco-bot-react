79369b41abb1d5dcd95c9373f299204e437d90a9
Martin <martinardiles.a@gmail.com>
2026-05-05 15:31:33 -0400

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App(){
           Recordatorios
         </Heading>
 
-        <Box p={6} w="xl" borderWidth={1} borderRadius="lg" boxShadow="sm">
+        <Box p={6} w="lg" borderWidth={1} borderRadius="lg" boxShadow="sm">
           <VStack spacing={4}>
             <Heading size="md" alignSelf="flex-start">Ingresar nueva tarea.</Heading>
 
